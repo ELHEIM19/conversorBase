f620217f9acf1fdfc04558ec88e9b0d1dcf6d915
ELHEIM19 <55957321+ELHEIM19@users.noreply.github.com>
2025-07-13 11:35:53 -0500

    subir proyecto

---
+++ conversorBase/Form1.Designer.vb
@@ -0,0 +1,154 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        GroupBox1 = New GroupBox()
+        RadioButton3 = New RadioButton()
+        RadioButton2 = New RadioButton()
+        RadioButton1 = New RadioButton()
+        TextBox1 = New TextBox()
+        TextBox2 = New TextBox()
+        Label1 = New Label()
+        Label2 = New Label()
+        Label3 = New Label()
+        GroupBox1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' GroupBox1
+        ' 
+        GroupBox1.Controls.Add(RadioButton3)
+        GroupBox1.Controls.Add(RadioButton2)
+        GroupBox1.Controls.Add(RadioButton1)
+        GroupBox1.Location = New Point(354, 21)
+        GroupBox1.Name = "GroupBox1"
+        GroupBox1.Size = New Size(250, 125)
+        GroupBox1.TabIndex = 0
+        GroupBox1.TabStop = False
+        GroupBox1.Text = "Base de Salida"
+        ' 
+        ' RadioButton3
+        ' 
+        RadioButton3.AutoSize = True
+        RadioButton3.Location = New Point(81, 87)
+        RadioButton3.Name = "RadioButton3"
+        RadioButton3.Size = New Size(117, 24)
+        RadioButton3.TabIndex = 2
+        RadioButton3.TabStop = True
+        RadioButton3.Text = "Hexadecimal"
+        RadioButton3.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton2
+        ' 
+        RadioButton2.AutoSize = True
+        RadioButton2.Location = New Point(81, 64)
+        RadioButton2.Name = "RadioButton2"
+        RadioButton2.Size = New Size(65, 24)
+        RadioButton2.TabIndex = 1
+        RadioButton2.TabStop = True
+        RadioButton2.Text = "Octal"
+        RadioButton2.UseVisualStyleBackColor = True
+        ' 
+        ' RadioButton1
+        ' 
+        RadioButton1.AutoSize = True
+        RadioButton1.Location = New Point(81, 43)
+        RadioButton1.Name = "RadioButton1"
+        RadioButton1.Size = New Size(77, 24)
+        RadioButton1.TabIndex = 0
+        RadioButton1.TabStop = True
+        RadioButton1.Text = "Binario"
+        RadioButton1.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(168, 60)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(125, 27)
+        TextBox1.TabIndex = 1
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Enabled = False
+        TextBox2.Location = New Point(168, 112)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(125, 27)
+        TextBox2.TabIndex = 2
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Location = New Point(62, 60)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(52, 20)
+        Label1.TabIndex = 3
+        Label1.Text = "Entero"
+        Label1.TextAlign = ContentAlignment.TopCenter
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Location = New Point(48, 112)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(75, 20)
+        Label2.TabIndex = 4
+        Label2.Text = "Resultado"
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Location = New Point(122, 21)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(128, 20)
+        Label3.TabIndex = 5
+        Label3.Text = "Conversr de Bases"
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8F, 20F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(646, 200)
+        Controls.Add(Label3)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(GroupBox1)
+        Name = "Form1"
+        Text = "Form1"
+        GroupBox1.ResumeLayout(False)
+        GroupBox1.PerformLayout()
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+
+End Class
---
+++ conversorBase/Form1.vb
@@ -0,0 +1,72 @@
+﻿Public Class Form1
+    Private Sub GroupBox1_Enter(sender As Object, e As EventArgs) Handles GroupBox1.Enter
+
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        TextBox1.Text = "10" ' Valor por defecto en el TextBox1
+    End Sub
+
+    'Funcion para pasar de decimal a otra base
+
+    Function DecimalToBase(decimalNumber As Integer, base As Integer) As String
+        Dim result As String = ""
+        Dim remainder As Integer
+        While decimalNumber > 0
+            remainder = decimalNumber Mod base
+            If remainder < 10 Then
+                result = remainder.ToString() & result
+            Else
+                result = Chr(remainder + 55) & result ' Convert to A, B, C, etc.
+            End If
+            decimalNumber \= base
+        End While
+        Return result
+    End Function
+
+    Private Sub RadioButton1_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton1.CheckedChanged
+
+        If RadioButton1.Checked Then
+            'captura el valor del TextBox1 y lo convierte a integer
+            Dim decimalNumber As Integer
+
+            decimalNumber = Convert.ToInt32(TextBox1.Text)
+            Dim base As Integer = 2 ' Base binaria
+            TextBox2.Text = DecimalToBase(decimalNumber, base)
+        End If
+    End Sub
+
+    Private Sub RadioButton2_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton2.CheckedChanged
+
+        If RadioButton2.Checked Then
+            'captura el valor del TextBox1 y lo convierte a integer
+            Dim decimalNumber As Integer
+
+            decimalNumber = Convert.ToInt32(TextBox1.Text)
+
+            Dim base As Integer = 8 ' Base octal
+            TextBox2.Text = DecimalToBase(decimalNumber, base)
+        End If
+
+
+    End Sub
+
+    Private Sub RadioButton3_CheckedChanged(sender As Object, e As EventArgs) Handles RadioButton3.CheckedChanged
+
+        If RadioButton3.Checked Then
+            'captura el valor del TextBox1 y lo convierte a integer
+            Dim decimalNumber As Integer
+
+            decimalNumber = Convert.ToInt32(TextBox1.Text)
+
+            Dim base As Integer = 16 ' Base hexadecimal
+            TextBox2.Text = DecimalToBase(decimalNumber, base)
+        End If
+
+
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+        TextBox2.Text = "" ' Limpiar el TextBox2 al cambiar el valor del TextBox1
+    End Sub
+End Class
